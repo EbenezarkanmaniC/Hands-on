ebee19c4eabc753acc21063e9a13460e45a086f8
Ebenezarkanmani.C <41163240+EbenezarkanmaniC@users.noreply.github.com>
2021-07-01 14:01:12 +0530

    Lombok,SONAR-3

---
+++ FlightManagement/src/main/java/com/cognizant/flightmanagement/models/Flight.java
@@ -0,0 +1,29 @@
+package com.cognizant.flightmanagement.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class Flight {
+	private String id;
+	private String flightType;
+	private List<Passenger> passengers= new ArrayList<>();
+
+	public boolean addPassenger(Passenger passenger) {
+		return passengers.add(passenger);
+	}
+
+	public boolean removePassenger(Passenger passenger) {
+		return passengers.remove(passenger);
+	}
+
+	public Object getFlightType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
